3183df3dad9843d9e08fae42af60789b81edc722
Olena Poroskun <56368022+alenaporoskun@users.noreply.github.com>
2024-08-28 09:46:22 +0300

    Update settings.py

--- image_web_classifier/settings.py
+++ image_web_classifier/settings.py
@@ -1,5 +1,8 @@
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,7 +12,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-oc^irpj3$!sjy&j$@q!4mq(!7&5k2tnc1urvjz$)x2cf46fl9a'
+# Get the SECRET_KEY from the .env file
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
