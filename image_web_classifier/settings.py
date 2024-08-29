5ea3d10c9688685c806c3b4c5d8f200cce63df6d
dspuliaiev <d.s.puliaiev@gmail.com>
2024-08-29 09:49:08 +0300

    set

--- image_web_classifier/settings.py
+++ image_web_classifier/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['static-inesita-pyweb-e2cbbeee.koyeb.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
