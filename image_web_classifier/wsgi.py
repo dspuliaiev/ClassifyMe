770b1b7ad63b1bdd3c8871170f0cde67253ab429
dspuliaiev <d.s.puliaiev@gmail.com>
2024-09-08 16:09:53 +0300

    Docker_54

--- image_web_classifier/wsgi.py
+++ image_web_classifier/wsgi.py
@@ -1,12 +1,3 @@
-"""
-WSGI config for image_web_classifier project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -14,3 +5,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'image_web_classifier.settings')
 
 application = get_wsgi_application()
+app= application
